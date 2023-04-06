60231d2dd6ba5ac9c80d5415b0b298d418b4f700
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-06 22:08:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
-chat_id = 1141722952
+chat_id = 1141722952 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = p
-    time = 50
-    return (2 * np.mean(x) / time ** 2 -  norm.ppf(1 - alpha / 2, scale=2) / time ** 2, 2 * np.mean(x) / time ** 2 - norm.ppf(alpha / 2, scale=2) / time ** 2)
+    alpha = 1 - p
+    n = len(x)
+    left = (max(x) - 1 / 2) / (50**2 / 2)
+    right = (-np.log(alpha) / n + max(x) - 1 / 2) / (50**2 / 2)
+    return left, right
+
